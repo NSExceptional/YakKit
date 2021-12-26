1bcea43b418f532e463d7f5d438a92da8aae6614
Tanner Bennett <tannerbennett@me.com>
2021-12-26 00:34:53 -0600

    Remove methods in Misc

--- Pod/Classes/Networking/YYClient+Misc.h
+++ Pod/Classes/Networking/YYClient+Misc.h
@@ -14,9 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface YYClient (Misc)
 
 #pragma mark Misc
-- (void)logEvent:(NSString *)event completion:(nullable YYErrorBlock)completion;
-- (void)refreshLocate:(nullable YYErrorBlock)completion;
-- (void)contactUs:(NSString *)topic message:(NSString *)message email:(NSString *)email completion:(nullable YYErrorBlock)completion;
+
 
 @end
 
--- Pod/Classes/Networking/YYClient+Misc.m
+++ Pod/Classes/Networking/YYClient+Misc.m
@@ -7,48 +7,10 @@
 //
 
 #import "YYClient+Misc.h"
-
+#import <Firebase/Firebase.h>
 
 @implementation YYClient (Misc)
 
-- (void)logEvent:(NSString *)event completion:(nullable YYErrorBlock)completion {
-    NSParameterAssert(event);
-    
-    NSDictionary *body = @{@"eventType": event,
-                           @"userID": self.userIdentifier};
-    
-    [self post:^(TBURLRequestBuilder *make) {
-        make.endpoint(kepLogEvent).bodyJSONFormString(body);
-    } callback:^(TBResponseParser *parser) {
-        [self handleStatus:parser.JSON callback:completion];
-    }];
-}
 
-- (void)refreshLocate:(nullable YYErrorBlock)completion {
-    NSDictionary *params = @{@"latitude": @(self.location.coordinate.longitude),
-                             @"longitude": @(self.location.coordinate.latitude)};
-    
-    [self unsignedGet:^(TBURLRequestBuilder *make) {
-        make.baseURL(kBaseContentURL).endpoint(kepRefreshersLocate).queries([self generalQuery:params]);
-    } callback:^(TBResponseParser *parser) {
-        YYRunBlockP(completion, parser.error);
-    }];
-}
-
-- (void)contactUs:(NSString *)category message:(NSString *)message email:(NSString *)email completion:(nullable YYErrorBlock)completion {
-    NSParameterAssert(category); NSParameterAssert(message); NSParameterAssert(email);
-    
-    NSDictionary *query = @{@"category": category,
-                            @"email": email,
-                            @"message": message,
-                            @"logs": [NSUUID UUID].UUIDString, // TODO idk what goes here, so
-                            @"userID": self.userIdentifier};
-    
-    [self post:^(TBURLRequestBuilder *make) {
-        make.endpoint(kepContactUs).bodyJSONFormString(query);
-    } callback:^(TBResponseParser *parser) {
-        YYRunBlockP(completion, parser.error);
-    }];
-}
 
 @end
